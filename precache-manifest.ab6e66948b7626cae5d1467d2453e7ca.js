bf18a6ed1a710f42499c2471b47154b1dd7ba274
“Daniel” <daniellimhong@gmail.com>
2020-03-13 16:57:54 -0700

    Updates

---
+++ precache-manifest.ab6e66948b7626cae5d1467d2453e7ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407dc607a0eea65bccf4586b831684aa",
+    "url": "/kanto-pokedex/index.html"
+  },
+  {
+    "revision": "37d0af9a246435a21870",
+    "url": "/kanto-pokedex/static/css/main.02566d64.chunk.css"
+  },
+  {
+    "revision": "ab010e08c0869fe8b1f5",
+    "url": "/kanto-pokedex/static/js/2.87460356.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/kanto-pokedex/static/js/2.87460356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d0af9a246435a21870",
+    "url": "/kanto-pokedex/static/js/main.95a1690a.chunk.js"
+  },
+  {
+    "revision": "ad9a75642d4169acb8ca",
+    "url": "/kanto-pokedex/static/js/runtime-main.f2e9b6e8.js"
+  }
+]);
